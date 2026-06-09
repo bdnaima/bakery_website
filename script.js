a2cd544ec0c5c8e2738185d936515cbaff248387
Naima Malik <noma_m@hotmail.com>
2026-06-09 21:00:23 +0200

    Create json file for products and add more images

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// Fetch cupcakes
+fetch("products.json")
+    // Fetch Cupcakes from products.json file
+    .then((response) => response.json()
+        .then((data) => {
+            const cupCakeContainer = document.getElementById("cupcake-container");
+            data.cupcakes.forEach(cupcake => {
+                cupCakeContainer.innerHTML += `
+                <div class="card">
+                    <img src="${cupcake.image}" alt="${cupcake.name}">
+                    <p>${cupcake.name}</p>
+                    <p>$${cupcake.price.toFixed(2)}</p>
+                </div>
+                `;
+            });
+
+            // Fetch wedding cakes from products.json file
+            const weddingCake = document.getElementById("wedding-cake-container");
+            data.weddingCakes.forEach(cake => {
+                weddingCake.innerHTML += `
+                <div class="card">
+                    <img src="${cake.image}" alt="${cake.name}">
+                    <p>${cake.name}</p>
+                    <p>$${cake.price}</p>
+                </div>
+                `
+            })
+        })).catch((error) => console.log(error));
